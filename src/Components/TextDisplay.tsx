35f24d157eed19d96e4f9b9b41cbdc201e27c416
Jay <jay.webp@gmail.com>
2023-06-11 11:45:21 +1000

    Add TextDisplay

---
+++ src/Components/TextDisplay.tsx
@@ -0,0 +1,13 @@
+/**
+ * Receives a string from parent and displays the string
+ * @param props 
+ * @returns 
+ */
+function TextDisplay(props: object) {
+
+    return (
+        <div>{}</div>
+    )
+}
+
+export default TextDisplay;
